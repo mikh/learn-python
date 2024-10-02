3a54a1fb608a4bacaec306855dc4a4cf28093bac
Mikhail Andreev <mikha.andreev@gmail.com>
2024-10-02 15:25:44 -0400

    Generator experiments

--- try_generator.py
+++ try_generator.py
@@ -22,11 +22,45 @@ def get_size_recursive(o: Any):
     return size
 
 
+def is_multiple_of_11(v: int) -> bool:
+    """Is it a multiple of 11"""
+    return v % 11 == 0
+
+
+def infinite_11s():
+    num = 0
+    while True:
+        if is_multiple_of_11(num):
+            print(f"{num} is a multiple of 11")
+            i = yield num
+            if i is not None:
+                print(f"set num = {i}")
+                num = i
+            else:
+                print("i is None")
+        num += 1
+        print(f"num = {num}")
+
+
+def eleven_loop():
+    ele_gen = infinite_11s()
+    counter = 0
+    for i in ele_gen:
+        print(i)
+        digits = len(str(i))
+        print(f"Send response = {ele_gen.send(10 ** (digits))}")
+        counter += 1
+        if counter > 4:
+            break
+
+
 if __name__ == "__main__":
     # for j in generate(300, 13):
     #     print(j)
-    num_squared_lc = [x**2 for x in range(100)]
-    num_squared_gc = (x**2 for x in range(100))
+    # num_squared_lc = [x**2 for x in range(100)]
+    # num_squared_gc = (x**2 for x in range(100))
 
-    print("lc", get_size_recursive(num_squared_lc))
-    print("gc", get_size_recursive(num_squared_gc))
+    # print("lc", get_size_recursive(num_squared_lc))
+    # print("gc", get_size_recursive(num_squared_gc))
+
+    eleven_loop()
